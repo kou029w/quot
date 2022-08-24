031553612fa3c5241f9de2dbaf92b9652a115a73
Kohei Watanabe <kou029w@gmail.com>
2022-08-24 15:27:10 +0900

    dedent

--- app/src/components/editor.tsx
+++ app/src/components/editor.tsx
@@ -4,18 +4,16 @@ import "./editor.css";
 export default (props: {
   id: number;
   onUpdatePage: (content: Pages.RequestContentPage) => void;
-}) => {
-  return (
-    <textarea
-      id={String(props.id)}
-      class="editor"
-      autofocus
-      onInput={(e) => {
-        const text = e.currentTarget.value;
-        const lines = text.split("\n");
-        props.onUpdatePage({ id: props.id, title: lines[0] ?? "", text });
-        e.currentTarget.setAttribute("rows", String(Math.max(2, lines.length)));
-      }}
-    ></textarea>
-  );
-};
+}) => (
+  <textarea
+    id={String(props.id)}
+    class="editor"
+    autofocus
+    onInput={(e) => {
+      const text = e.currentTarget.value;
+      const lines = text.split("\n");
+      props.onUpdatePage({ id: props.id, title: lines[0] ?? "", text });
+      e.currentTarget.setAttribute("rows", String(Math.max(2, lines.length)));
+    }}
+  ></textarea>
+);
